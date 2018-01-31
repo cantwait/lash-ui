9cbee21dbf3db5c7d89f40a1e28aa78de21c9ee4
Rafael Cadenas <cadenas.rafael@gmail.com>
2018-01-31 00:03:57 -0500

    dialogo usuario nuevo

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -38,18 +38,73 @@
       color="pink"
       dark
       fixed
-      @click.stop="dialog = !dialog"
+      @click.stop="cuDialog = !cuDialog"
     >
       <v-icon>add</v-icon>
     </v-btn>
-    <v-dialog v-model="dialog" persistent max-width="290">
+    <!-- delete diaglo -->
+    <v-dialog v-model="dialogDelete" persistent max-width="290">
       <v-card>
         <v-card-title class="headline">Esta seguro de esta acción?</v-card-title>
         <v-card-text>Tenga en cuenta que esta acción no se puede reversar.</v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="red darken-1" flat @click.native="onDeletePerson">Si</v-btn>
-          <v-btn color="blue darken-1" flat @click.native="dialog = !dialog">Cancelar</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="dialogDelete = !dialogDelete">Cancelar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <!-- new dialog  -->
+    <v-dialog v-model="cuDialog" persistent max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Legal first name" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Legal last name" hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Email" required></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Password" type="password" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-select
+                  label="Age"
+                  required
+                  :items="['0-17', '18-29', '30-54', '54+']"
+                ></v-select>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-select
+                  label="Interests"
+                  multiple
+                  autocomplete
+                  chips
+                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                ></v-select>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="cuDialog = !cuDialog">Cerrar</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="cuDialog = !cuDialog">Guardar</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -61,7 +116,7 @@
       return {
         userToDelete: null,
         dialogDelete: false,
-        dialogNew: false,
+        cuDialog: false,
         items: [
           { icon: 'person', iconClass: 'grey lighten-1 white--text', id: 1, title: 'Rafael Cadenas', subtitle: 'Jan 9, 2014' },
           { icon: 'person', iconClass: 'grey lighten-1 white--text', id: 2, title: 'Estefany Alvarado', subtitle: 'Jan 17, 2014' },

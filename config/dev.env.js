c6c017ddd086350cf6e0427c1661e76a4607687b
Rafael Cadenas <cadenas.rafael@gmail.com>
2018-03-22 21:29:18 -0500

    updating conf

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,4 @@ var prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  PUSHER_KEY:'"5795fd77db4198bd9713"',
-  PUSHER_CLUSTER:'"us2"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,5 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  PUSHER_KEY:'"5795fd77db4198bd9713"',
+  PUSHER_CLUSTER:'"us2"'
 }
